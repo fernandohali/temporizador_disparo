ed3ceff2c9622d3e723e57b89b2daccd1fb776a6
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2025-02-02 23:09:41 -0300

    Colocando os comentários no projeto.

--- temporizador_disparo.c
+++ temporizador_disparo.c
@@ -11,26 +11,17 @@
 #define LED_PIN_3 13
 #define INTERVAL_MS 3000
 
-static volatile uint8_t counter = 0;
-static volatile bool is_alarm_enabled = false;
+static volatile uint8_t led_counter = 0;
+static volatile bool alarm_active = false;
 
-static void handle_button_press(uint gpio, uint32_t events);
-static bool update_semaphore(struct repeating_timer *_);
+static void button_press_handler(uint gpio, uint32_t events);
+int64_t led_control_callback(alarm_id_t _id, void *_);
 
-int64_t callback(alarm_id_t _id, void *_) {
-	gpio_put(LED_PIN_1, counter <= 0);
-	gpio_put(LED_PIN_2, counter <= 1);
-	gpio_put(LED_PIN_3, counter <= 2);
+int main()
+{
+	stdio_init_all();
 
-	is_alarm_enabled = (counter < 3);
-
-	counter++;
-	return is_alarm_enabled ? (INTERVAL_MS * 1000) : 0;
-}
-
-int main() {
-    stdio_init_all();
-
+	// Configuração dos LEDs como saída
 	gpio_init(LED_PIN_1);
 	gpio_set_dir(LED_PIN_1, GPIO_OUT);
 
@@ -44,27 +35,48 @@ int main() {
 	gpio_put(LED_PIN_2, 0);
 	gpio_put(LED_PIN_3, 0);
 
+	// Configuração do botão como entrada com pull-up ativado
 	gpio_init(BUTTON_PIN);
 	gpio_set_dir(BUTTON_PIN, GPIO_IN);
 	gpio_pull_up(BUTTON_PIN);
-	gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &handle_button_press);
+	gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &button_press_handler);
 
+	// Loop principal
 	while (true)
 		sleep_ms(10000);
 }
 
-static void handle_button_press(uint gpio, uint32_t events) {
+// Função para lidar com o pressionamento do botão
+static void button_press_handler(uint gpio, uint32_t events)
+{
 	static volatile uint32_t last_time = 0;
 
-	if (gpio != BUTTON_PIN) return;
-    uint32_t current_time = to_us_since_boot(get_absolute_time());
+	if (gpio != BUTTON_PIN)
+		return;
+	uint32_t current_time = to_us_since_boot(get_absolute_time());
 
-	if (current_time - last_time > DEBOUNCING_TIME_US) {
-		if (!gpio_get(BUTTON_PIN) && !is_alarm_enabled) {
-			counter = 0;
-			is_alarm_enabled = true;
-			add_alarm_in_ms(1, callback, NULL, false);
+	// Verifica se o botão foi pressionado após o tempo de debounce
+	if (current_time - last_time > DEBOUNCING_TIME_US)
+	{
+		if (!gpio_get(BUTTON_PIN) && !alarm_active)
+		{
+			led_counter = 0;
+			alarm_active = true;
+			add_alarm_in_ms(1, led_control_callback, NULL, false);
 		}
 		last_time = current_time;
 	}
+}
+
+// Callback para controlar os LEDs em intervalos definidos
+int64_t led_control_callback(alarm_id_t _id, void *_)
+{
+	gpio_put(LED_PIN_1, led_counter <= 0);
+	gpio_put(LED_PIN_2, led_counter <= 1);
+	gpio_put(LED_PIN_3, led_counter <= 2);
+
+	alarm_active = (led_counter < 3);
+
+	led_counter++;
+	return alarm_active ? (INTERVAL_MS * 1000) : 0;
 }
